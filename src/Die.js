4be6163c3fe42f00dd7524e19e68e6cabfbd235d
Christopher Haverman <chaverma@alumni.uci.edu>
2016-09-17 13:46:46 -0700

    Successful click handler for faces.

--- src/Die.js
+++ src/Die.js
@@ -4,19 +4,51 @@
 
 import React, { PropTypes } from 'react';
 
+class DieFace extends React.Component {
+    static propTypes: {
+        value: PropTypes.number.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            highlightClass: ''
+        };
+    }
+
+    render() {
+        return (
+          <span className={`col-md-1 text-center ${this.state.highlightClass}`}
+                onClick={() => this._onClick()}>
+              {this.props.value}
+          </span>
+        );
+    }
+
+    _onClick() {
+        this.setState({
+            highlightClass: this._toggleHighlight(this.state.highlightClass)
+        });
+    }
+
+    _toggleHighlight(currentValue) {
+        return currentValue === '' ? 'bg-primary' : '';
+    }
+}
+
 class Die extends React.Component {
+    static propTypes: {
+        faces: PropTypes.number.isRequired
+    };
 
     render() {
         return (
             <div className="center-block">
-                {[...new Array(this.props.faces)].map((e, i) => <span className="col-md-1 text-center">{i}</span>)}
+                {[...new Array(this.props.faces)].map((e, i) => <DieFace key={i} value={i}/>)};
             </div>
         );
     }
 }
-
-Die.propTypes = {
-    faces: PropTypes.number.isRequired
-};
 
 export default Die;

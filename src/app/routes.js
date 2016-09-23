57bd77155f8865a8140da4785233e1b1e80faf07
Christopher Haverman <chaverma@alumni.uci.edu>
2016-09-22 23:30:13 -0700

    Refactoring and adding docblocks

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,41 +3,54 @@
  **/
 
 /**
- * Applies common setup to the responses by our API.
- *
- * @param response Express response configure.
+ * Provides a standard way of providing responses from our routes.
  **/
-function configureResponse(response) {
-    response.set('Content-Type', 'application/json');
-}
+class RouteHandler {
+    constructor(request, response) {
+        this.request = request;
+        this.response = response;
+        this.response.set('Content-Type', 'application/json');
+    }
 
-function makeError(message) {
-    return JSON.stringify({status: 'error', message: message});
-}
+    /**
+     * Provides a JSON-encoded string containing an error.
+     *
+     * @param message String message to encode.
+     **/
+    makeError(message) {
+        return JSON.stringify({status: 'error', message: message});
+    }
 
-function makeResult(value, message) {
-    return JSON.stringify({status: 'ok', data: value, message: message});
+    /**
+     * Provides a JSON-encoded string containing the result.
+     *
+     * @param value Any object to include in the response
+     * @param message String message
+     * @return a JSON encoded string representing our response.
+     **/
+    makeResult(value, message) {
+        return JSON.stringify({status: 'ok', data: value, message: message});
+    }
 }
 
 function Roll(request, response) {
-    configureResponse(response);
+    let handler = new RouteHandler(request, response);
 
     response.status(400);
     if (!request.query.hasOwnProperty('pool')) {
-        let error = makeError('You must specify how many dice to roll via the \'pool\' query ' +
-            'parameter.');
+        let error = handler.makeError('You must specify how many dice to roll via the \'pool\' ' +
+            'query parameter.');
         response.send(error);
         return;
     }
     let numberOfDice = parseInt(request.query.pool);
     if (Number.isNaN(numberOfDice)) {
-        response.send(makeError('\'pool\' must be an integer.'));
+        response.send(handler.makeError('\'pool\' must be an integer.'));
         return;
     }
 
     response.status(200);
-
-    response.send(makeResult([1, 2, 3, 4], 'No message'));
+    response.send(handler.makeResult([1, 2, 3, 4], 'No message'));
 }
 
 export default Roll;

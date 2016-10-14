f1629b49f082458859a7999dcee00304b38c267a
Christopher Haverman <chaverma@alumni.uci.edu>
2016-10-13 23:47:12 -0700

    Preparing a release candidate

--- src/app/routes/main.js
+++ src/app/routes/main.js
@@ -4,16 +4,17 @@
  **/
 import Roll from './roll';
 
+/**
+ * Provides the routes available in this app.
+ **/
 class Router {
     constructor(api_path) {
-        self._api_path = api_path;
+        this._api_path = api_path;
     }
 
     get routes() {
         return [{handler: Roll, path: `${this._api_path}/roll`}];
     }
-
-
 }
 
 export default Router;
